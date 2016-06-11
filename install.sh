b74b33a21241478806d1d64e718e6400ad24e24b
Luuk Veenis <luukveenis@gmail.com>
2016-06-11 13:32:52 -0700

    Add i3 config

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -fs ~/src/dotfiles/ssh/config ~/.ssh/config
 ln -fs ~/src/dotfiles/vim/.vimrc ~/
 ln -fs ~/src/dotfiles/tmux/.tmux.conf ~/
 ln -fs ~/src/dotfiles/tmux/.tmux.theme ~/
+ln -fs ~/src/dotfiles/i3/config ~/.i3/
